7a71aa2abe9b7d2fd21fa9b13f9cc87337f1985d
Michael Brett <michael.brett.webdev@gmail.com>
2018-10-12 21:20:17 +0100

    chore: update dependencies, rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
-import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
 
 export default [
-    {
-        input: 'src/main.js',
-        external: [],
-        output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'esm' }
-        ],
-        plugins: [
-            babel({
-                exclude: 'node_modules/**/*'
-            })
-        ]
-    }
+  {
+    input: "src/main.js",
+    external: ["fs", "path"],
+    output: [{ file: pkg.main, format: "cjs" }, { file: pkg.module, format: "esm" }],
+    plugins: [
+      babel({
+        exclude: "node_modules/**/*"
+      }),
+      resolve(),
+      commonjs()
+    ]
+  }
 ];
